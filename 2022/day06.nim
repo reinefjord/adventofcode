d04337ce03c0330c9869f29107b03839b11727ff
Rupus Reinefjord <rupus@reinefjord.net>
2022-12-06 19:27:24 +0100

    day06: handle no match

--- 2022/day06.nim
+++ 2022/day06.nim
@@ -1,11 +1,12 @@
 proc decode(data: string, unique: int): int =
-  for i in 0..data.high:
+  for i in 0..data.len - unique:
     block inner:
       let chars = data[i..i + unique - 1]
       for j in 0..unique - 2:
         if chars[j] in chars[j+1..^1]:
           break inner
       return i + unique
+  return -1
 
 let data = stdin.readAll()
 echo decode(data, 4)
